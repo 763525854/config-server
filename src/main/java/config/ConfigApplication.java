0d63f566d3538a18d103df937a2af9f328417b68
依韵 <763525854@qq.com>
2019-04-17 06:27:45 +0800

    引入spring eureka

--- src/main/java/config/ConfigApplication.java
+++ src/main/java/config/ConfigApplication.java
@@ -2,9 +2,11 @@ package config;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
+@EnableDiscoveryClient
 @SpringBootApplication(scanBasePackages="config")
 public class ConfigApplication {
 	public static void main(String[] args) {
